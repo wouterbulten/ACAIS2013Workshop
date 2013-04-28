f077666bbd8575aaada1655eb1367cba10fad154
Wouter Bulten <wouterbulten@mai-ai.org>
2013-04-28 18:39:10 +0200

    Set the shape of an agent to something different.

--- agents.py
+++ agents.py
@@ -16,6 +16,11 @@ class RandomAgent (breve.Wanderer):
 		breve.Wanderer.__init__(self)
 
 		self.setWanderRange(breve.vector(10, 0, 10))
+
+		cube = breve.createInstances(breve.Cube, 1).initWith(breve.vector(1,1,1))
+
+		self.setShape(cube)
+
 		print "Created agent"
 
 	def iterate(self):
